e2ad2d18f0a777658f7b5913c275bb725cc31d1e
souzafcharles <souzach@outlook.com>
2022-10-18 18:24:04 -0300

    Exercise 04

---
+++ Exercise04/src/TesteSelenium.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TesteSelenium {
+    public static void main(String[] args) throws InterruptedException {
+        String projectPath = System.getProperty("user.dir");
+        String chromeDriverPath = projectPath + "\\lib\\chromedriver\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://localhost/TC1/exercice04.html");
+        driver.manage().window().maximize();
+        WebElement dropDown = driver.findElement(By.xpath("//*[@id=\"gatos\"]"));
+        if (dropDown.isDisplayed()) {
+            System.out.println("A lista está sendo mostrada!");
+        } else {
+            System.out.println("A lista não está sendo mostrada!");
+        }
+        if (dropDown.isEnabled()) {
+            System.out.println("Está habilitado!");
+        }else{
+            System.out.println("Não está habilitado!");
+        }
+        Select ddList = new Select((dropDown));
+
+        ddList.selectByVisibleText("Cinza");
+        ddList.selectByValue("B");
+        ddList.selectByIndex(2);
+    }
+}
+
